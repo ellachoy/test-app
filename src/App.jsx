1d0b6eec34d2a046950916443b68b0a28da04742
ella choy <egal21de@gmail.com>
2024-06-17 22:06:39 +0200

    WIP: Erzeugt test aufgaben

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Main from './components/main'
+import Sidebar from './components/sidebar'
+
+function App() {
+  return (
+    <section className='section wrapper'>
+      <Sidebar />
+      <Main />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <h3>Sidebar</h3>
+      <form>
+        <div>
+          <label>Sidebar menu1</label>
+          <input type='text' />
+        </div>
+        <div>
+          <label>Sidebar menu2</label>
+          <input type='text' />
+        </div>
+      </form>
+    </div>
+  )
+}
